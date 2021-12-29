80bc0cd94bd42d809f96c3cc198cb46670300a07
sbx_user1051 <sbx_user1051@169.254.153.149>
2021-12-29 18:06:20 +0000

    Wed Dec 29 18:06:20 UTC 2021

---
+++ src/BeFaster.App.Tests/Solutions/SUM/HloSolutionTest.cs
@@ -0,0 +1,28 @@
+﻿using BeFaster.App.Solutions.SUM;
+using NUnit.Framework;
+using System;
+
+namespace BeFaster.App.Tests.Solutions.SUM
+{
+    [TestFixture]
+    public class SumSolutionTest
+    {
+        [TestCase(-1, 0)]
+        [TestCase(101, 0)]
+        [TestCase(0, -1)]
+        [TestCase(0, 101)]
+        public void WhenInputIsInvalid_ThrowError(int x, int y)
+        {
+            Assert.Throws<ArgumentException>(() => SumSolution.Sum(x, y));
+        }
+
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 1)]
+        [TestCase(55, 61, ExpectedResult = 116)]
+        public int WhenInputIsValid_ReturnInputsSummed(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
+
+    }
+}
