07203d20dddb857efdff4ceb793d19582e9cd355
sbx_user1051 <sbx_user1051@169.254.153.149>
2021-12-29 18:08:33 +0000

    Wed Dec 29 18:08:33 UTC 2021

--- src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -1,28 +1,21 @@
-﻿using BeFaster.App.Solutions.SUM;
+﻿using BeFaster.App.Solutions.HLO;
+using BeFaster.App.Solutions.SUM;
 using NUnit.Framework;
 using System;
 
 namespace BeFaster.App.Tests.Solutions.SUM
 {
     [TestFixture]
-    public class SumSolutionTest
+    public class HelloSolutionTest
     {
-        [TestCase(-1, 0)]
-        [TestCase(101, 0)]
-        [TestCase(0, -1)]
-        [TestCase(0, 101)]
-        public void WhenInputIsInvalid_ThrowError(int x, int y)
+        [Test]
+        public void WhenCalled_ReturnHelloWorld()
         {
-            Assert.Throws<ArgumentException>(() => SumSolution.Sum(x, y));
-        }
+            var result = HelloSolution.Hello("");
 
-        [TestCase(0, 0, ExpectedResult = 0)]
-        [TestCase(1, 0, ExpectedResult = 1)]
-        [TestCase(55, 61, ExpectedResult = 116)]
-        public int WhenInputIsValid_ReturnInputsSummed(int x, int y)
-        {
-            return SumSolution.Sum(x, y);
+            Assert.AreEqual("hello to the world", result);
         }
 
     }
 }
+
