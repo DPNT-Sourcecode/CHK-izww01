025f6698bf4dad3053daf8d250ba9c9c84e08fca
sbx_user1051 <sbx_user1051@169.254.153.149>
2021-12-29 17:59:21 +0000

    Wed Dec 29 17:59:21 UTC 2021

--- src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -9,10 +9,14 @@ namespace BeFaster.App.Tests.Solutions.SUM
     {
 
         [TestCase(-1, 0)]
+        [TestCase(101, 0)]
+        [TestCase(0, -1)]
+        [TestCase(0, 101)]
         public void WhenInputIsInvalid_ThrowError(int x, int y)
         {
             Assert.Throws<ArgumentException>(() => SumSolution.Sum(x, y));
         }
     }
 }
+
 
--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -1,4 +1,5 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System;
 
 namespace BeFaster.App.Solutions.SUM
 {
@@ -6,7 +7,19 @@ namespace BeFaster.App.Solutions.SUM
     {
         public static int Sum(int x, int y)
         {
-            throw new SolutionNotImplementedException();
+            if (x < 0 || x > 100)
+            {
+                throw new ArgumentException($"{nameof(x)} must be between 0 - 100", nameof(x));
+            }
+
+
+            if (y < 0 || y > 100)
+            {
+                throw new ArgumentException($"{nameof(y)} must be between 0 - 100", nameof(y));
+            }
+
+            return 0;
         }
     }
 }
+
